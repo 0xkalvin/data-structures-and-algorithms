0fc932a2d4abe4bbcb292400aff49a63833a7636
kalvin <kalvinvini1998@gmail.com>
2019-10-19 16:04:43 -0300

    vectors exercises

--- algorithms/vectors.c
+++ algorithms/vectors.c
@@ -1,39 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <limits.h>
 
-bool sameSmallest(int v1[10], int v2[10]){
+#include "binarySearch.c"
+#include "sorting/mergeSort.c"
 
-    int m1 = v1[0];
-    int m2 = v2[0];
+/*  
+    So this is an compilation of exercises that involves doing something with 
+    an array
+*/
+
+
+/*  Checks if two passed arrays have the same smallest number  */
+int sameSmallest(int arr1[10], int arr2[10]){
+
+    int s1 = arr1[0];
+    int s2 = arr2[0];
 
     for (int i = 0; i < 10; i++)
     {
-        if(v1[i] < m1){
-            m1 = v1[i];
+        if(arr1[i] < s1){
+            s1 = arr1[i];
         }
 
-        if(v2[i] < m2){
-            m2 = v2[i];
+        if(arr2[i] < s2){
+            s2 = arr2[i];
         }
     }
     
-    printf(" %d \n", m1);
-    printf(" %d \n", m2);
-
-    return m1 == m2;
+    return s1 == s2;
 }
 
 
+/*  
+    returns the smallest number that's in both passed array if there's 
+    at least one element in common
+ */
+int smallestCommon(int arr1[], int arr2[], int size1, int size2){
 
+    /*  sorts first array*/
+    mergeSort(arr1, 0, size1 - 1);
 
-int main(void){
+    int s = INT_MAX;
 
+    /*  
+        For each element in arr2, binary searches it in arr1.
+        If found, checks if is less than the others found.
+    */
+    for(int i = 0; i < size2; i++){
 
-    int v1[10] = {1,2,3,4,-3,6,7,8,9,10};
-    int v2[10] = {1,2,3,4,5,6,7,8,-1,10};
-
-    printf(" %d ", sameSmallest(v1, v2));
-
-
+        if(binarySearchRecursive(arr1, 0, 9, arr2[i]) >= 0){
+            if(arr2[i] < s){
+                s = arr2[i];
+            }
+        }
+    }
+    
+    return s;
 }
+
+
+int onlyOneSwap(int v[10]){
+
+
+    int temp[10];
+    for(int i = 0; i < 9; i++){
+        temp[i] = v[i];
+    }
+
+    mergeSort(temp, 0, 9);
+
+    int c = 0;
+    for(int i = 0; i < 9; i++){
+
+        if(v[i] > v[i + 1]){
+            c++;
+        }
+
+    }
+    return c;
+}
+
